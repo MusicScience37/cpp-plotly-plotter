f093935522650283c7993605a232363cfb71a961
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-03-15 23:29:45 +0900

    Generated initial files.

---
+++ doc/sphinx/src/conf.py
@@ -0,0 +1,48 @@
+"""Configuration file for Sphinx."""
+
+# pylint: disable=invalid-name
+# pylint: disable=redefined-builtin
+
+# -- Project information -----------------------------------------------------
+
+project = "cpp-plotly-plotter"
+release = "unknown"  # inserted in command line
+
+copyright = "2025, Kenta Kabashima. "
+author = "Kenta Kabashima"
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.todo",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+
+exclude_patterns = []
+
+# -- Options for PlantUML ----------------------------------------------------
+
+extensions += ["sphinxcontrib.plantuml"]
+
+plantuml_output_format = "svg"
+plantuml_syntax_error_image = True
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "sphinx_orange_book_theme"
+html_static_path = ["_static"]
+html_title = project
+html_theme_options = {
+    "show_prev_next": False,
+    "logo": {
+        "text": html_title,
+    },
+    "pygment_light_style": "gruvbox-light",
+    "pygment_dark_style": "native",
+    "repository_url": "https://gitlab.com/MusicScience37Projects/utility-libraries/cpp-plotly-plotter",
+    "use_repository_button": True,
+}
+
+html_static_path = ["_static"]
---
+++ doc/sphinx/src/index.rst
@@ -0,0 +1,28 @@
+cpp-plotly-plotter
+==========================================
+
+A library to create plots using plotly.
+
+Developed at
+`https://gitlab.com/MusicScience37Projects/utility-libraries/cpp-plotly-plotter <https://gitlab.com/MusicScience37Projects/utility-libraries/cpp-plotly-plotter>`_.
+
+API reference can be viewed `here <api/index.html>`_.
+
+.. toctree::
+    :maxdepth: 1
+    :caption: About
+
+    license
+
+.. toctree::
+    :maxdepth: 1
+    :caption: Usage
+
+    how_to_use
+
+.. toctree::
+    :maxdepth: 1
+    :caption: Development
+
+    how_to_build_in_repository
+    convention
