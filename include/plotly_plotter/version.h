717b61ab09a7ffecfe27b91a52e923ed9501bb88
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-08-14 04:48:15 +0000

    :books: Prepare for release v0.7.0.

--- include/plotly_plotter/version.h
+++ include/plotly_plotter/version.h
@@ -23,7 +23,7 @@
 #define PLOTLY_PLOTTER_VERSION_MAJOR 0
 
 //! Minor version of this project.
-#define PLOTLY_PLOTTER_VERSION_MINOR 6
+#define PLOTLY_PLOTTER_VERSION_MINOR 7
 
 //! Patch version of this project.
 #define PLOTLY_PLOTTER_VERSION_PATCH 0
